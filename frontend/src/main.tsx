41549466206ff904d52fa7a976cdd0ae709ce0e0
ChristopherSemyonov <mellybang.lav@gmail.com>
2024-11-12 18:16:11 +0200

    Fetch products

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,6 +12,10 @@ import './index.css'
 import App from './App.tsx'
 import HomePage from './pages/HomePage.tsx'
 import ProductPage from './pages/ProductPage.tsx'
+import axios from 'axios'
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
